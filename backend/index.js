2a768d6ec4ef7c1410df271a9064278138b2e116
Saivishwaram Ramkumar <saivishwaram.ramkumar@gmail.com>
2025-07-28 20:03:12 +0530

    chore: basic npm installs were made

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// index.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
